6f177d4f31ff2d6c343e1db033711572eba16660
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-22 16:27:59 +0200

    end commit

--- src/Esercizio3/Main.java
+++ src/Esercizio3/Main.java
@@ -1,67 +1,64 @@
 package Esercizio3;
-import java.util.Date;
+
 public class Main {
-    public class e-CommerceApp(){
+    public static class eCommerceApp {
 
-        static class articolo(){
-
+        static class Articolo {
             private int codiceArticolo;
-
             private String descrizione;
-
             private double prezzo;
+            private int disponibilita;
 
-            private int disponibilità;
-
-            public articolo(int codiceArticolo , String descrizione , double prezzo,  int disponibilità ){
-                this.codiceArticolo= codiceArticolo;
-                this.descrizione= descrizione;
-                this.prezzo= prezzo;
-                this.disponibilità=disponibilità;
+            public Articolo(int codiceArticolo, String descrizione, double prezzo, int disponibilita) {
+                this.codiceArticolo = codiceArticolo;
+                this.descrizione = descrizione;
+                this.prezzo = prezzo;
+                this.disponibilita = disponibilita;
             }
         }
-static class Cliente (){
+
+        static class Cliente {
             private int codiceCliente;
-                    private String nome;
-                    private String cognome;
-                    private  String email;
-                    private Date dataIscrizione;
+            private String nome;
+            private String cognome;
+            private String email;
+            private int dataIscrizione;
 
-                    public  Cliente( int codiceCliente , String nome , String cognome , String email ,Date dataIscrizione){
-                        this.codiceCliente= codiceCliente;
-                        this.nome=nome;
-                        this.cognome= cognome;
-                        this.email=email;
-                        this.dataIscrizione=dataIscrizione;
-                    }
-}
-static class Carrello(){
+            public Cliente(int codiceCliente, String nome, String cognome, String email, int  dataIscrizione) {
+                this.codiceCliente = codiceCliente;
+                this.nome = nome;
+                this.cognome = cognome;
+                this.email = email;
+                this.dataIscrizione = dataIscrizione;
+            }
+        }
+
+        static class Carrello {
             private Cliente cliente;
-            private articolo[] elencoArticoli;
-            private  double totaleCostiArticoli;
+            private Articolo[] elencoArticoli;
+            private double totaleCostiArticoli;
 
-    public Carrello(Cliente cliente, articolo[] elencoArticoli) {
-        this.cliente = cliente;
-        this.elencoArticoli = elencoArticoli;
-        this.totaleCostiArticoli = calcolaTotaleCostoArticoli();
+            public Carrello(Cliente cliente, Articolo[] elencoArticoli) {
+                this.cliente = cliente;
+                this.elencoArticoli = elencoArticoli;
+                this.totaleCostiArticoli = calcolaTotaleCostoArticoli();
+            }
+
+            private double calcolaTotaleCostoArticoli() {
+                double totale = 0;
+                for (Articolo articolo : elencoArticoli) {
+                    totale += articolo.prezzo;
+                }
+                return totale;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        eCommerceApp.Articolo articolo1 = new eCommerceApp.Articolo(2356, "Iphone", 600.00, 4);
+        eCommerceApp.Articolo articolo2=new eCommerceApp.Articolo(2354, "Samsung" ,320.00,2 );
+        eCommerceApp.Cliente cliente1 = new eCommerceApp.Cliente(35464, "Carlo", "Patalano", "Patalanocarlo674@gmail.com" , 22/05/2024);
+        eCommerceApp.Articolo[] articoliNelCarrello = {articolo1, articolo2};
+        eCommerceApp.Carrello carrelloCliente1 = new eCommerceApp.Carrello(cliente1, articoliNelCarrello);
+        System.out.println("Il cliente:" + cliente1.nome + " " + cliente1.cognome + " " + "ha totalizzato una spesa complessiva di:" + " " +carrelloCliente1.totaleCostiArticoli + " euro."  );
+    }
 }
-private double calcolaTotaleCostoArticoli() {
-    double totale = 0;
-    for (articolo articolo : elencoArticoli){
-        totale= articolo.prezzo;
-    }
-    return totale;
-}
-
-    }
-
-        public static void main (String[] args) {
-            articolo articolo1= new articolo(2356 , "Iphone" ,600.00, 4);
-                 Cliente cliente1 = new Cliente(35464, "Carlo" ,"Patalano","Patalanocarlo674@gmail.com",new Date());
-            articolo[] articoliNelCarrello= {articolo1};
-            Carrello carrellocliente1 = new Carrello(cliente1, articoliNelCarrello );
-            System.out.println("Totale della spesa del carrello è:" + carrellocliente1.totaleCostiArticoli);
-        }}
-    }
-
-
